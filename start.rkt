b0b10738a3c9b0104687bbcfe50121adcab10f63
Danil Annenkov <support@ib-soft.ru>
2011-12-29 17:29:53 +0900

    Writing pid to file.

--- start.rkt
+++ start.rkt
@@ -1,8 +1,10 @@
 #lang racket
 (require web-server/dispatch 
-         web-server/servlet-env)
+         web-server/servlet-env
+         mzlib/os)
 (require "dispatch.rkt")
 
+(with-output-to-file "pid" (lambda () (write (getpid))) #:exists 'replace)
 (serve/servlet url-dispatch
                #:port 8080
                #:command-line? #t
