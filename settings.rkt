eb37a734db57e694aa69977909975393fcebc96c
Danil Annenkov <support@ib-soft.ru>
2012-01-25 22:33:30 +0900

    Fixed urls and paths (now related). Added pid-file setting.

--- settings.rkt
+++ settings.rkt
@@ -1,8 +1,10 @@
 #lang racket
 (require racket/runtime-path)
 
+(define-runtime-path pid-file "./pid")
 (define-runtime-path log-file "sample_log/payworld.log")
 (define passwords #hash((#"admin" . #"sample")))
 
-(provide log-file
+(provide pid-file 
+         log-file
          passwords)
--- start.rkt
+++ start.rkt
@@ -2,9 +2,10 @@
 (require web-server/dispatch 
          web-server/servlet-env
          mzlib/os)
-(require "dispatch.rkt")
+(require "dispatch.rkt"
+         "settings.rkt")
 
-(with-output-to-file "pid" (lambda () (write (getpid))) #:exists 'replace)
+(with-output-to-file pid-file (lambda () (write (getpid))) #:exists 'replace)
 (serve/servlet url-dispatch
                #:port 8080
                #:command-line? #t
